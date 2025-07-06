57e1024a58c40521e9565c184d4a117782acb837
Tim171717 <tfessler@student.ethz.ch>
2025-07-06 21:08:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ token = st.secrets["github_token"]
 g = Github(token)
 repo = g.get_repo("Tim171717/test")
 
-df = pd.read_csv('Plan_U13A_2526HR.csv')
+plan = 'Plan_U13A_2526HR.csv'
+df = pd.read_csv(plan)
 df['date'] = pd.to_datetime(df['date'])
 
 # Update rows where date matches
@@ -22,8 +23,8 @@ updated_content = df.to_csv(index=False)
 
 # Push the updated content back to GitHub
 repo.update_file(
-    path=file_path,
-    message=f"Updated {file_path} from Streamlit app",
+    path=plan,
+    message=f"Updated {plan} from Streamlit app",
     content=updated_content,
     sha=file.sha,
     branch="main"
